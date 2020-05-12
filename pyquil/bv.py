3d424dc4e2a95f14969925056efa367b0dfbddf9
Siddharth Joshi <sjoshi804@gmail.com>
2020-05-12 01:49:46 -0700

    project reorg, added teammates code

---
+++ pyquil/bv.py
@@ -0,0 +1,131 @@
+import numpy as np
+import random
+
+from pyquil import get_qc, Program
+from pyquil.quil import DefGate
+from pyquil.gates import *
+from pyquil.api import local_forest_runtime
+
+
+class Solver(object):
+
+    def __init__(self, f, n):   
+        self.f = f
+        self.n = n
+
+        self.__build_circuit()
+
+    def __generate_bit_strings(self, n):
+        '''
+        Input: n
+        Output: A list of bit strings Ex. n=2 -> ["00", "01", "10", "11"]
+
+        A recursive function to generate all possible bit strings with size n.
+        '''
+        if n==1:
+            return ["0", "1"]
+        else:
+            return ["0"+x for x in self.__generate_bit_strings(n-1)]+["1"+x for x in self.__generate_bit_strings(n-1)]
+
+    def __get_tensor(self, bit_string):
+        if bit_string=="0":
+            return np.array([1,0], dtype=np.int16)
+        elif bit_string=='1':
+            return np.array([0,1], dtype=np.int16)
+        else:
+            return np.tensordot(self.__get_tensor(bit_string[0]), self.__get_tensor(bit_string[1:]), 0).ravel().reshape(2**len(bit_string), -1)
+
+    def __modify_bit_string(self, bit_string):
+        res = self.f(bit_string[:-1])
+        if res==0:
+            return bit_string
+        elif res==1:
+            if bit_string[-1]=="0":
+                return bit_string[:-1]+"1"
+            elif bit_string[-1]=="1":
+                return bit_string[:-1]+"0"
+
+    def __produce_u_f_gate(self):
+        bit_strings = self.__generate_bit_strings(self.n+1)
+        xs = list()
+        bs = list()
+        for bit_string in bit_strings:
+            xs.append(self.__get_tensor(bit_string))
+            modified_bit_string = self.__modify_bit_string(bit_string)
+            bs.append(self.__get_tensor(modified_bit_string))
+        
+        X = np.hstack(tuple(xs))
+        B = np.hstack(tuple(bs))
+        
+        A = np.linalg.solve(X, B)
+        self.__u_f = np.array(A)
+        
+        self.__u_f_definition = DefGate("U_f", self.__u_f)
+        self.__U_f = self.__u_f_definition.get_constructor()
+        
+    def __build_circuit(self):
+        self.__produce_u_f_gate()
+        
+        self.__p = Program()
+        self.__p+=X(self.n)
+        for i in range(self.n+1):
+            self.__p += H(i)
+        self.__p+=self.__u_f_definition
+        self.__p +=self.__U_f(*range(self.n+1))
+        for i in range(self.n):
+            self.__p += H(i)
+        print(self.__p)
+    
+    def solve(self):
+        print('Value of b: {}'.format(self.f("0"*self.n)))
+        with local_forest_runtime():
+            qc = get_qc('9q-square-qvm')
+            result = qc.run_and_measure(self.__p, trials = 10)
+            for i in range(self.n):
+                print(result[i])
+        
+
+def random_bit_string_generator(n=1):
+    '''
+    Generates a random bit string of length n
+
+    Input: n (Default: n=1)
+    Output: A bit string Ex. n=7 -> "0101010"
+    '''
+    bit_string = ''
+    for i in range(0,n):
+        bit_string+=str(random.choice([0,1]))
+    return bit_string
+
+def inner_product_mod_2(x,y):
+    '''
+    Calculates inner product from two given bit strings
+    and then applies mod of 2
+
+    Input: x, y (Bit strings)
+    Output: Inner Product mod 2 Ex. x="1011" y="1101" -> (1+0+0+1)%2 = 2%2 = 0.
+    '''
+    # x, y are bit strings
+    sum=0
+    for i,j in zip(x,y):
+        sum+=int(i)*int(j)
+    return sum%2
+
+def addition_mod_2(x,y):
+    '''
+    Adds two bits and then applied mod 2
+
+    Input: x, y (Bits)
+    Output: Sum mod 2 Ex. x=1, y=1 -> (1+1)%2 = 2%2 = 0.
+    '''
+    #x, y are bits which are integers 0 or 1
+    return (x+y)%2
+
+#Test function with a completely randomized value of a and b.
+n = 3
+#random_bit_string_generator(len(x))
+#random.choice([0,1])
+f = lambda x: addition_mod_2(inner_product_mod_2("101", x), 1)
+
+solver = Solver(f, n)
+solver.solve()
---
+++ pyquil/grover.py
@@ -0,0 +1,89 @@
+import numpy as np
+
+from pyquil import get_qc, Program
+from pyquil.quil import DefGate
+from pyquil.gates import *
+from pyquil.api import local_forest_runtime
+
+
+class Solver(object):
+
+    def __init__(self, f, n):   
+        self.f = f
+        self.n = n
+        self.k = int(np.floor((np.pi/4)*np.sqrt(2**n)))
+
+        self.__build_circuit()
+
+    def __generate_bit_strings(self, n):
+        '''
+        Input: n
+        Output: A list of bit strings Ex. n=2 -> ["00", "01", "10", "11"]
+
+        A recursive function to generate all possible bit strings with size n.
+        '''
+        if n==1:
+            return ["0", "1"]
+        else:
+            return ["0"+x for x in self.__generate_bit_strings(n-1)]+["1"+x for x in self.__generate_bit_strings(n-1)]
+
+    def __produce_z_0_gate(self):
+        z_0 = np.identity(2**n)
+        z_0[0][0] = -z_0[0][0]
+        self.__z_0_definition = DefGate("Z_0", z_0)
+        self.__Z_0 = self.__z_0_definition.get_constructor()
+
+
+    def __produce_z_f_gate(self):
+        z_f = np.identity(2**n)
+        bit_strings = self.__generate_bit_strings(self.n)
+        for bit_string in bit_strings:
+            output = f(bit_string)
+            if output == 1:
+                i = bit_strings.index(bit_string)
+        #i = np.random.randint(2**n)
+        z_f[i][i] = -z_f[i][i]
+        self.__z_f_definition = DefGate("Z_f", z_f)
+        self.__Z_f = self.__z_f_definition.get_constructor()
+
+    def __produce_negative_gate(self):
+        negative =  -np.identity(2**n)
+        self.__negative_definition = DefGate("NEGATIVE", negative)
+        self.__NEGATIVE = self.__negative_definition.get_constructor()
+        
+    def __build_circuit(self):
+        self.__produce_z_f_gate()
+        self.__produce_z_0_gate()
+        self.__produce_negative_gate()
+
+        G=Program()
+        G += self.__z_f_definition
+        G += self.__Z_f(*range(self.n))
+        for i in range(self.n):
+            G += H(i)
+        G+=self.__z_0_definition
+        G+=self.__Z_0(*range(self.n))
+        for i in range(self.n):
+            G += H(i)
+        G+=self.__negative_definition
+        G+=self.__NEGATIVE(*range(self.n))
+
+        self.__p = Program()
+        for i in range(self.n):
+            self.__p += H(i)
+        for i in range(self.k):
+            self.__p+=G
+    
+    def solve(self):
+        with local_forest_runtime():
+            qc = get_qc('9q-square-qvm')
+            result = qc.run_and_measure(self.__p, trials = 10)
+            for i in range(self.n):
+                print(result[i])
+
+
+n=3
+f = lambda x: 1 if x=="101" else 0
+
+solver = Solver(f, n)
+print(solver.solve())
